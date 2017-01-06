0ce89f931695c478c1068d803ffdb40c131818a6
Tom Rothe <info@tomrothe.de>
2017-01-06 15:27:33 +0100

    finish marquee

--- ledhat.ino
+++ ledhat.ino
@@ -1,4 +1,7 @@
 #include <Adafruit_NeoPixel.h>
+
+#include "letters.h"
+
 #define STRIP_WIDTH 300
 #define HAT_WIDTH 36
 #define HAT_HEIGHT 8
@@ -7,39 +10,10 @@
 Adafruit_NeoPixel strip =
     Adafruit_NeoPixel(STRIP_WIDTH, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-
-const uint8_t LETTERS[][8] = {
-  {
-    B11111111,
-    B00010000,
-    B00010000,
-    B00010000,
-    B00010000,
-    B00010000,
-    B00010000,
-    B00010000,
-  },
-  {
-    B00000000,
-    B01111110,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01111110,
-    B00000000,
-  },
-  {
-    B00000000,
-    B1100011,
-    B1010101,
-    B1001001,
-    B1000001,
-    B1000001,
-    B1000001,
-    B1000001,
-  }
-};
+const uint8_t MARQUEE_LETTERS[] = {0, 1, 2, 2, 2, 1};
+const uint32_t MARQUEE_COLOR = strip.Color(255, 255, 0);
+#define MARQUEE_DELAY 100 // milliseconds between frames
+const uint8_t MARQUEE_LETTER_COUNT = sizeof(MARQUEE_LETTERS) / sizeof(MARQUEE_LETTERS[0]);
 
 void setup() {
   strip.begin();
@@ -50,45 +24,82 @@ void setup() {
 
 int i = 0;
 
+class Mode {
+public:
+  Mode(){};
+  // if loop returns a true, it means that this mode is done
+  virtual bool loop() { return NULL; };
+  virtual void enter(){}; // called when the state is entered
+  virtual void leave(){}; // called when the state is left
+protected:
+  // sets the color for the given coordinates.
+  // negative/out of bounds values are allowed and ignored.
+  void setPixel(int8_t x, int8_t y, uint32_t color) {
+    if ((x < 0) || (x >= HAT_WIDTH)) // ignore out of bounds
+      return;
+    if ((y < 0) || (y >= HAT_HEIGHT))
+      return;
+    strip.setPixelColor((HAT_HEIGHT - 1 - y) * HAT_WIDTH + x, color);
+  }
+};
 
-// sets the color for the given coordinates.
-// negative/out of bounds values are allowed and ignored.
-void setPixel(int8_t x, int8_t y, uint32_t color) {
-  if ((x < 0) || (x >= HAT_WIDTH)) // ignore out of bounds
-    return;
-  if ((y < 0) || (y >= HAT_HEIGHT))
-    return;
-  strip.setPixelColor((HAT_HEIGHT-1-y) * HAT_WIDTH + x, color);
-}
+class MarqueeMode : public Mode {
+public:
+  void enter() {
+    // initialize the position
+    reset_position();
+  }
+  void reset_position() {
+    // we start with the letters beyond the screen on the right
+    position = HAT_WIDTH;
+  }
 
-// looks up the letter with the given index and draws it with the given color.
-// the x_offset determines how much the letter should be moved to the left or right.
-// negative offsets are valid (means shift to the left leading to a partial letter).
-// partial letters are also drawn and out of bounds coordinates are skipped.
-void drawLetter(int8_t x_offset, uint8_t letter_index, const uint32_t color) {
-  if ((x_offset < -7) || (x_offset >= HAT_WIDTH)) // skip out of bounds
-    return;
-  for (uint8_t j = 0; j < 8; j++) { // y
-    for (uint8_t i = 0; i < 8; i++) { // x
-      uint8_t mask = 1 << i;
-      if (LETTERS[letter_index][j] & mask) {
-        setPixel(x_offset + i, j, color);
+  bool loop() {
+    strip.clear();
+    uint32_t color = strip.Color(255, 0, 0);
+    for (int i = 0; i < MARQUEE_LETTER_COUNT; i++) {
+      drawLetter(position + i * 8, MARQUEE_LETTERS[i], MARQUEE_COLOR);
+    }
+    strip.show();
+    position--;
+    if (position < (-(int)MARQUEE_LETTER_COUNT * 8))
+      reset_position();
+    delay(MARQUEE_DELAY);
+    return false;
+  }
+
+  // looks up the letter with the given index and draws it with the given color.
+  // the x_offset determines how much the letter should be moved to the left or
+  // right.
+  // negative offsets are valid (means shift to the left leading to a partial
+  // letter).
+  // partial letters are also drawn and out of bounds coordinates are skipped.
+  void drawLetter(int8_t x_offset, uint8_t letter_index, const uint32_t color) {
+    if ((x_offset < -7) || (x_offset >= HAT_WIDTH)) // skip out of bounds
+      return;
+    for (uint8_t j = 0; j < 8; j++) {   // y
+      for (uint8_t i = 0; i < 8; i++) { // x
+        uint8_t mask = 1 << i;
+        if (LETTER_IMAGES[letter_index][j] & mask) {
+          setPixel(x_offset + i, j, color);
+        }
       }
     }
   }
-}
 
+private:
+  int position;
+};
+
+MarqueeMode marquee_mode_instance = MarqueeMode();
+Mode *current_mode = NULL;
+// Mode* mode_sequence
 void loop() {
-  strip.clear();
-  uint32_t color = strip.Color(255, 0, 0);
-  drawLetter(0, 0, color);
-  drawLetter(8, 1, color);
-  drawLetter(16, 2, color);
-  // static uint8_t cur = 0;
-  // for (int i = 0; i < HAT_HEIGHT; i++) {
-  //   setPixel(cur, i, color);
-  // }
-  // cur = (cur+1) % HAT_WIDTH;
-  strip.show();
-  delay(200);
+  if (current_mode == NULL) {
+    current_mode = &marquee_mode_instance;
+    current_mode->enter();
+  }
+
+  // bool mode_done = current_mode->loop();
+  // if
 }
